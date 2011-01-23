3ce66d3bc7ea89490a6feda07b911105f556db81
Steve Boj <steveboj@yahoo.com>
2011-01-23 17:42:34 -0500

    password stuff

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe User do
   before(:each) do
-    @attr = {:name => "Example User", :email => "user@example.com"}
+    @attr = {:name => "Example User", :email => "user@example.com", :password => "foobar", :password_confirmation => "foobar"}
   end
   
   it "should create a new instance given a valid attribute" do
@@ -53,5 +53,57 @@ describe User do
     user = User.new(@attr)
     user.should_not be_valid
   end  
+  
+  describe "passwords" do
+    
+    before(:each) do
+      @user = User.new(@attr)
+    end
+    
+    it "should have a password attribute" do
+      User.new(@attr).should respond_to(:password)
+    end
+    
+    it "shoud have a password confirmation attribute" do
+      User.new(@attr).should respond_to(:password_confirmation)
+    end
+    
+  end
+  
+  describe "password validations" do
+    
+    it "should require a password" do
+      User.new(@attr.merge(:password => "", :password_confirmation => "")).should_not be_valid
+    end
+    
+    it "should require a matching password confirmation" do
+      User.new(@attr.merge(:password_confirmation => "invalid")).should_not be_valid
+    end
+    
+    it "should reject short passwords" do
+      short = "a" * 5
+      hash = @attr.merge(:password => short, :password_confirmation => short)
+      User.new(hash).should_not be_valid
+    end
+    
+    it "should reject long passwords" do
+      long = "a" * 41
+      hash = @attr.merge(:password => long, :password_confirmation => long)
+      User.new(hash).should_not be_valid
+    end
+        
+    describe "password encryption" do
+      
+      before (:each) do
+        @user = User.create!(@attr)
+      end
+      
+      it "should have an encrpted password attribute" do
+        @user.should respond_to(:encrypted_password)
+      end
+      
+    end
+  end
+  
   
 end
