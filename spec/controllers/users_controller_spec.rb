9c3bf4bd9ffba17780513034a7ecae0e804c3507
Steve Boj <steveboj@yahoo.com>
2011-01-24 23:37:08 -0500

    Completed sign in out

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -103,6 +103,13 @@ describe UsersController do
         post :create, :user => @attr
         flash[:success].should =~ /Welcome to the Sample App!/i
       end
+      
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
+      
+      
     end
   
   end
